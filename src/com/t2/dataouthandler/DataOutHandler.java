a14b4c117c6af653f8599be365d02022b78d5056
scott.coleman <scott.coleman@tee2.org>
2013-08-26 13:36:55 -0700

    Changed to V2.1.1

--- src/com/t2/dataouthandler/DataOutHandler.java
+++ src/com/t2/dataouthandler/DataOutHandler.java
@@ -110,7 +110,7 @@ import com.t2.drupalsdk.UserServices;
 public class DataOutHandler  implements JREngageDelegate {
 					
 	private static final String TAG = DataOutHandler.class.getName();
-	private static final String VERSION_STRING = "2.1.0";
+	private static final String VERSION_STRING = "2.1.1";
 	
 	//private static final String DEFAULT_REST_DB_URL 	= "http://gap.t2health.org/and/phpWebservice/webservice2.php";	 
 	// private static final String DEFAULT_REST_DB_URL 	= "http://gap.t2health.org/and/json.php";	 
